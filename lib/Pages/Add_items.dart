68564aad2e2ec3d8222ddbbcd196fbacf4e6904c
manav <toremanav@gmail.com>
2023-07-18 16:48:15 +0530

    Added textfromfield

--- lib/Pages/Add_items.dart
+++ lib/Pages/Add_items.dart
@@ -8,11 +8,24 @@ class Additem extends StatefulWidget {
 }
 
 class _AdditemState extends State<Additem> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add places'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(label: Text('Place')),
+                )
+              ],
+            )),
       ),
     );
   }
