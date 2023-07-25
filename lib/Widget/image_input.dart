f3cd4e3751161749ac652b643b74a1ff90dc5777
manav <toremanav@gmail.com>
2023-07-25 21:24:32 +0530

    modified

--- lib/Widget/image_input.dart
+++ lib/Widget/image_input.dart
@@ -11,6 +11,10 @@ class _ImageinputState extends State<Imageinput> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+          border: Border.all(
+              width: 1,
+              color: Theme.of(context).colorScheme.primary.withOpacity(0.2))),
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
